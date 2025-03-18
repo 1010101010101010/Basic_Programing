5d315d5f9c4baf937d8d0ba92383178c30ceaeaa
Pradeep_Oram <oram.pradeep2@gmail.com>
2025-03-18 20:35:11 +0530

    My_approach_for_Permutation
    My approach for Permutation which is easy but not good for Time Complexity

---
+++ Basic_Programing/Permutation_Easy_Bad.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programing
+{
+    class Permutation_Easy_Bad
+    {
+        // Method to print all permutations of the string
+        public void PrintPermutations(string str)
+        {
+            GeneratePermutations(str.ToCharArray());
+            //Time Complexity: O(n^2) and only works for this specific case
+        }
+
+        // Helper method to generate and print permutations using backtracking
+        private void GeneratePermutations(char[] arr)
+        {
+            // First loop of permutations
+            PrintPermutations(arr);
+
+            // Swap and then print permutations again
+            Swap(arr, 0, 2);
+            PrintPermutations(arr);
+        }
+
+        private void PrintPermutations(char[] arr)
+        {
+            foreach (var item in arr)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(item);
+
+                foreach (var item1 in arr)
+                {
+                    if (item != item1)
+                    {
+                        sb.Append(item1);
+                    }
+                }
+
+                Console.WriteLine(sb.ToString());
+            }
+        }
+
+
+        // Method to swap two elements in the array
+        private void Swap(char[] arr, int i, int j)
+        {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
